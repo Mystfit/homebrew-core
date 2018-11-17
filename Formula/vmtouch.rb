01c0f65890e407b9b93ddc90b98078010de677db
Igor Kapkov <igasgeek@me.com>
2018-11-17 18:08:48 +1100

    vmtouch 1.3.1

--- Formula/vmtouch.rb
+++ Formula/vmtouch.rb
@@ -1,8 +1,8 @@
 class Vmtouch < Formula
   desc "Portable file system cache diagnostics and control"
   homepage "https://hoytech.com/vmtouch/"
-  url "https://github.com/hoytech/vmtouch/archive/v1.3.0.tar.gz"
-  sha256 "4615980b8f824c8eb164e50ec0880bcb71591f4e3989a6075e5a3e2efd122ceb"
+  url "https://github.com/hoytech/vmtouch/archive/v1.3.1.tar.gz"
+  sha256 "d57b7b3ae1146c4516429ab7d6db6f2122401db814ddd9cdaad10980e9c8428c"
   head "https://github.com/hoytech/vmtouch.git"
 
   bottle do
