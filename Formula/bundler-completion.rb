981a337bfdec563b0811443d8d6e0243a6ba6f37
Igor Kapkov <igasgeek@me.com>
2018-11-27 12:22:53 +1100

    bundler-completion 20170723

--- Formula/bundler-completion.rb
+++ Formula/bundler-completion.rb
@@ -2,11 +2,13 @@ class BundlerCompletion < Formula
   desc "Bash completion for Bundler"
   homepage "https://github.com/mernen/completion-ruby"
   url "https://github.com/mernen/completion-ruby.git",
-    :revision => "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
-  version "2"
+    :revision => "710d0699ee710e181da90c4f3af8b39e4b5c81de"
+  version "20170723"
   head "https://github.com/mernen/completion-ruby.git"
 
   bottle :unneeded
+
+  depends_on "bash-completion"
 
   def install
     bash_completion.install "completion-bundle" => "bundler"
