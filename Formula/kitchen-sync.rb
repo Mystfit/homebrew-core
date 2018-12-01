b0e5ea1a18742abc5a08cc24463b730926b07279
Igor Kapkov <igasgeek@me.com>
2018-12-01 23:14:45 +1100

    kitchen-sync 1.7

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -1,8 +1,8 @@
 class KitchenSync < Formula
   desc "Fast efficiently sync database without dumping & reloading"
   homepage "https://github.com/willbryant/kitchen_sync"
-  url "https://github.com/willbryant/kitchen_sync/archive/1.5.tar.gz"
-  sha256 "73843b3fde113ac29fdfdaf316fadb7e10abd3d01e953abcb7af497126dd357c"
+  url "https://github.com/willbryant/kitchen_sync/archive/1.7.tar.gz"
+  sha256 "c8f393056290de5488d46b975a574638bb33c2d1f697d4902f0d46f662112f86"
   head "https://github.com/willbryant/kitchen_sync.git"
 
   bottle do
